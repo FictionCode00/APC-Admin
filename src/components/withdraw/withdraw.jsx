8d18a3920c483d609fdc207640c3f402d56ddd6a
khanmosajjid <mosajjid.khan@gmail.com>
2023-05-24 12:43:13 +0530

    withdraw

--- src/components/withdraw/withdraw.jsx
+++ src/components/withdraw/withdraw.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react"
 import { getWithdrawRequests } from "../../services/CommonServices"
 import { useState } from "react"
-import { Button, Table } from "react-bootstrap"
+import { Button, Table, Toast } from "react-bootstrap"
 import { BiCheck, BiX } from "react-icons/bi"
+import TronWeb from 'tronweb'; 
 
 const WithdrawManagement=()=>{
 
@@ -18,6 +19,36 @@ const WithdrawManagement=()=>{
     useEffect(()=>{
         getWithdrawTranscList()
     },[])
+
+    const onHandleWithdraw=async(data)=>{
+         try{
+            let tronWeb1 = new TronWeb({
+                fullHost: "https://api.trongrid.io",
+                solidityNode:"https://api.trongrid.io",
+                eventServer: "https://api.trongrid.io",
+                privateKey:"a9dd75fdfc07da3626812d96546f9684ecafc525aa1eafd55d2c55136acca4df"
+               
+              });
+
+              const contract = await tronWeb1.contract().at(TA9KzpuyxiAFzBwfh8SK2ygo6SLCXdqifU);
+      
+              const amountWithDecimals = data.amount * Math.pow(10, 6);
+        
+              const transaction = await contract.transfer(data.toAddress, amountWithDecimals).send({
+                shouldPollResponse: true,
+                feeLimit: 1e8, // Set the maximum amount of TRX to spend on transaction fees
+              });
+              if(transaction){
+                Toast.success("Amount Transfer Success");
+              }
+              
+              return transaction;
+
+         }catch(e){
+            Toast.error("Amount Transfer Failed ")
+         }
+    }
+
     return (
         <>
              <div class="content_area">
@@ -39,7 +70,14 @@ const WithdrawManagement=()=>{
                                     <td>{transaction.address}</td>
                                     <td>{transaction.amount}</td>
                                     <td className="text-center d-flex justify-content-center">
-                                        <Button className="customBtn bg-success border-0" type="button" >
+                                        <Button className="customBtn bg-success border-0" type="button" onClick={()=>{
+                                            let data={
+                                                name:transaction.fullname,
+                                                address:transaction.address,
+                                                amount:transaction.amount
+                                            }
+                                            onHandleWithdraw(data)
+                                        }}>
                                             <BiCheck />
                                         </Button>
                                         <Button className="customBtn ms-2 border-0" type="button">
